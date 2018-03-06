94c1302573c58b41a9e5b03bc177932c5b09c29c
ScnuWang <scnu_wang@163.com>
2018-03-06 15:06:53 +0800

    vue-router基础使用

--- src/main.js
+++ src/main.js
@@ -1,14 +1,40 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+// 新增vue-router  https://router.vuejs.org/zh-cn/
+import Vue from "vue"
+import VueRouter from "vue-router"
+Vue.use(VueRouter);
 
-Vue.config.productionTip = false
+const Home = {template:'<div>首页内容</div>'};
+const First = {template:'<div>First内容</div>'};
+const Second = {template:'<div>Second内容</div>'};
 
-/* eslint-disable no-new */
+
+const routes = [
+  {path:'/',component:Home},
+  {path:'/first',component:First},
+  {path:'/second',component:Second}
+];
+
+const router = new VueRouter({
+  model:'history',
+  routes : routes //可以简写成routes
+});
+
 new Vue({
-  el: '#app',
-  // components: { App },
-  // template: '<App/>', 使用render函数代替
-  render:x => x(App)
-})
+  router,
+  template:'<div id="r"><h1>导航</h1><ul><li><router-link to="/">Home</router-link></li><li><router-link to="/first">First</router-link><li><router-link to="/second">Second</router-link></li></ul><router-view class="abc"></router-view></div>'
+}).$mount('#app');
+
+// import Vue from "vue"
+// import App from './App'
+//
+// Vue.config.productionTip = false
+//
+// /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   // components: { App },
+//   // template: '<App/>', Vue2.x可使用render函数代替
+//   render:x => x(App)
+// })
