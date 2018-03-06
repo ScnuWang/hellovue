0aebda5bcb540cba88e40225933b105133f1b897
ScnuWang <scnu_wang@163.com>
2018-03-06 11:27:11 +0800

    基于webpack的基础模板，并在首页添加两个自定义组件

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <HelloVue/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import HelloVue from './components/HelloVue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,HelloVue
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
